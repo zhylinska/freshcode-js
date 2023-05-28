bee5bd9f3f385f94ea349f299964ed409866a441
Zhylinska <irynazhylinska83@gmail.com>
2023-05-28 19:01:43 +0200

    Make 6 tasks with if from homework

---
+++ welcome_js/index.js
@@ -0,0 +1,142 @@
+/*let variable = null;
+console.log(variable);
+
+let variable2 = 5;
+console.log(variable2);
+
+let variable3 = '2';
+console.log(variable3);
+
+let variable4 = variable2 + variable3;
+console.log(variable4);
+
+let variable5 = variable3 - variable2;
+console.log(variable5);
+
+let variable6 = null + variable2;
+console.log(variable6);
+
+let variable7 = 10 + undefined;
+console.log(variable7);
+
+const motto = "vvvvvvvvvvvvvvv:\"vvvvvvvvvvvvvv!\"(vvvvvvvv).";
+console.log(motto);
+
+let devide = 5 / 3;
+console.log(devide); 
+
+let r = 12;
+if(r > 12) {
+    r = 'r is 12';
+} 
+else if(r < 5) {
+    r = 'less than 5';
+} 
+else {
+    r = 'between 5 and 12'
+}
+console.log(r);
+
+
+ 
+const number = 10;
+
+if(number !== 5) {
+    console.log('number is not 5');
+}
+else {
+    console.log('number is 5');
+}
+*/
+/* task 1 */
+let min = 30;
+if (min >= 1 && min <= 15) {
+    console.log('first quarter');
+}
+else if(min > 15 && min <= 30) {
+    console.log('second quarter');
+}
+else if(min > 30 && min <= 45) {
+    console.log('third quarter');
+}
+else {
+    console.log('forth quarter')
+}
+
+/*task 2 */
+firstNumber = 8;
+secondNumber = 7;
+if(firstNumber > secondNumber) {
+    console.log('firstNumber is the biggest number');
+}
+if(firstNumber < secondNumber) {
+    console.log('secondNumber is the biggest number');
+}
+
+/*task 3 */
+let year = 2005;
+if((year % 4 === 0) && (year % 100 != 0) && (year % 400 === 0)) {
+    console.log('it\'s a leap year')
+}
+else {
+    console.log('it\'s a nonleap year')
+}
+
+/*task 4 */
+let start = 15;
+let end = 30;
+let number = 18;
+if((number >= start) && (number <= end))
+{
+    console.log('in range')
+}
+else {
+    console.log('not in range')
+}
+
+/*task 5 */
+let pens = 5;
+let notebooks = 6;
+let participants = 11;
+if((participants <= pens) && (participants <= notebooks)) {
+    console.log('enough presents')
+}
+else {
+    console.log('not enough presents')
+}
+
+/*task 6 */
+let age = 35;
+if(age < 6) {
+    console.log('priceless')
+}
+else if((age >= 6) && (age <=12)) {
+    console.log('price is 10$')
+}
+else {
+    console.log('price is 20$')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
