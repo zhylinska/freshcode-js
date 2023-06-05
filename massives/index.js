efd9b575a6f9c86073a9670310f8fe33fc73557d
Zhylinska <irynazhylinska83@gmail.com>
2023-06-05 18:07:39 +0200

    Make 2task massive

--- massives/index.js
+++ massives/index.js
@@ -1,7 +1,20 @@
+/*
 const array = [1,2,3,4,5,6,7];
-
 for(let i = 0; i < array.length; i++) {
     if (array[i] % 2 === 0) {
         console.log(array[i]);
     }
 }
+*/
+
+const arr = [3, 6, 8, 2, 3, 5, 1, 3];
+
+function sumArray(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+console.log(sumArray(arr));
+
