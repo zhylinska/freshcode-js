07d30f10016845c89076f5e0b3b0fb42390c4a3c
Zhylinska <irynazhylinska83@gmail.com>
2023-06-07 17:06:11 +0200

    make tasks with massives functions

---
+++ massive_practice/index.js
@@ -0,0 +1,45 @@
+//task1
+const massive1 = [1, 2, 3, 4, 5];
+const massive2 = [6, 7, 8, 9, 10];
+console.log(massive1.concat(massive2));
+//task2
+const massive3 = [1, 2, 3];
+console.log(massive3.reverse());
+//task3
+const massive4 = [3, 4, 5];
+massive4.push(4, 8, 10);
+console.log(massive4);
+//task4
+const massive5 = ['aaa', 'bbb', 'ccc'];
+console.log(massive5.pop());
+//task5
+const massive6 = ['3', 7, 10];
+massive6.unshift(2, 8);
+console.log(massive6);
+//task6
+const massive7 = ['3', 7, 10];
+console.log(massive7.join(' '));
+//task7
+const arr = [2, 44, 11, 234, 8, 2, 4, 1];
+function sum(a) {
+    console.log(a + 100);
+}
+arr.forEach(sum);
+//task8
+const massive8 = [1, 2, 3];
+const massive9 = [10, 11, 12];
+console.log(massive8.concat(massive9));
+//task9
+const massive10 = [1, 2, 3];
+massive10.push(7, 8, 9);
+console.log(massive10);
+//task10
+const  massive11 = [1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+console.log(massive11.slice(2, 5));
+//task11
+const massive12 = ['a', 'b', 'c', 'd'];
+massive12.reverse();
+console.log(massive12.join(''));
+  
+
+  
