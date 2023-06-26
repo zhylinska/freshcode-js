0bc1ed37cf08c8f99fe76999565ab5b588766934
Zhylinska <irynazhylinska83@gmail.com>
2023-06-26 19:10:26 +0200

    on click button - deleted on click

--- dom/index.js
+++ dom/index.js
@@ -1,8 +1,18 @@
-function getAlert() {
-    alert('hello user');
+function getAlert(event) {
+    const btn = event.target;
+   if(btn.style.backgroundColor === 'red') {
+       btn.style.backgroundColor = 'unset';
+       
+   }
+   else {
+       btn.style.backgroundColor = 'red'
+   }
+   
+    console.log(event);
 }
 const buttonElement = document.getElementById('btn');
 buttonElement.addEventListener('click', getAlert);
+
 
 //
 
@@ -11,3 +21,13 @@ function getLog() {
 }
 const buttonElement2 = document.getElementById('btn2');
 buttonElement2.addEventListener('mouseenter', getLog);
+
+//
+
+function getDeleted(event) {
+    const btn = event.target;
+    btn3.style.display = 'none';
+    }  
+    
+const buttonElement3 = document.getElementById('btn3');
+buttonElement3.addEventListener('click', getDeleted);
