a04a55912edfca1b65114769356cdeebd3f4fd1b
Zhylinska <irynazhylinska83@gmail.com>
2023-06-30 17:30:58 +0200

    code refactoring

---
+++ elements_creating/data.js
@@ -0,0 +1,34 @@
+
+const data = [
+    {
+        id: 1,
+        name: 'john',
+        description: 'user 1',
+        profilePicture: 'https://pixsector.com/cache/94bed8d5/av3cbfdc7ee86dab9a41d.png'
+    },
+    {
+        id: 2,
+        name: 'josh',
+        description: 'user 2',
+        profilePicture: 'https://pixsector.com/cache/94bed8d5/av3cbfdc7ee86dab9a41d.png'
+    },
+    {
+        id: 3,
+        name: 'jane',
+        description: 'user 3',
+        profilePicture: 'https://pixsector.com/cache/94bed8d5/av3cbfdc7ee86dab9a41d.png'
+    },
+    {
+        id: 4,
+        name: 'jonny',
+        description: 'user 4',
+        profilePicture: 'https://pixsector.com/cache/94bed8d5/av3cbfdc7ee86dab9a41d.png'
+    },
+    {
+        id: 5,
+        name: 'tracy',
+        description: 'user 5',
+        profilePicture: 'https://pixsector.com/cache/94bed8d5/av3cbfdc7ee86dab9a41d.png'
+    }
+];
+
