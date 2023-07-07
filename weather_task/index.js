3f33f6ad30bb6948cc5957b78e2307627bce2776
Zhylinska <irynazhylinska83@gmail.com>
2023-07-07 17:30:10 +0200

    make cards to weather widget

--- weather_task/index.js
+++ weather_task/index.js
@@ -6,11 +6,14 @@ make weather widget:
 -show result to user
  */
 
-const API_BASE = ''
-const API_KEY = ''
+
+const API_KEY = 'f7c576ba3699bdd0b98ddcf196639992';
+const API_BASE = 'https://api.openweathermap.org/data/2.5/weather';
+
 
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', buttonClickHandler);
+
 function buttonClickHandler({target}) {
     const selectValue = target.previousElementSibling.value;
     requestAPI((selectValue));
@@ -19,16 +22,39 @@ function buttonClickHandler({target}) {
 function requestAPI(city) {
     //make request
     //prepare URL
-    const url =  `${API_BASE}?q=${city}&appid=${API_KEY}&units=metric`;
-    fetch(url)  
+    const url = `${API_BASE}?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
             displayWeather(data);
         })
-    }
-    function displayWeather(weatherData) {
+}
+
+function displayWeather(weatherData) {
+    const {name, main: {temp, pressure}, weather: {description}, wind: {speed}} = weatherData;
+
+    const article = document.createElement('article');
     
-    }
+    const city = document.createElement('p');
+    city.append('city:', name);
+    
+    const temperature = document.createElement('p');
+    temperature.append('temperatur:', temp);
+    
+    const press = document.createElement('p');
+    press.append('pressure:', pressure);
+    
+    const wind = document.createElement('p');
+    wind.append('wind:', speed);
+
+    article.append(city, temperature, pressure, wind);
+    
+    
+    const section = document.querySelector('.wrapper');
+    section.append(article);
+
+
+}
     
